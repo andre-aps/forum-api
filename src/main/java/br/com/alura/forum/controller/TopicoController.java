4bac06ab6cefafd3f3d2d4484ea30bcbaa2c0965
André <andre.gmaps@gmail.com>
2021-02-18 21:51:50 -0300

    adicionando métodos novos no controller

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -3,15 +3,24 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.dto.AtualizacaoTopicoDto;
+import br.com.alura.forum.dto.DetalhesTopicoDto;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.dto.TopicoFormDto;
 import br.com.alura.forum.modelo.Topico;
@@ -29,7 +38,7 @@ public class TopicoController {
 	CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> listaTopicos(String nomeCurso) {
+	public List<TopicoDto> listar(String nomeCurso) {
 		if (nomeCurso != null) {
 			return TopicoDto.converte(topicoRepository.findByCursoNome(nomeCurso));
 		} else {
@@ -37,11 +46,49 @@ public class TopicoController {
 			return TopicoDto.converte(lista);
 		}
 	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if(!topico.isPresent())
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok().body(new DetalhesTopicoDto(topico));
+	}
 
 	@PostMapping
-	public ResponseEntity<TopicoDto> salvarTopico(@RequestBody TopicoFormDto form) throws URISyntaxException {
+	@Transactional
+	public ResponseEntity<TopicoDto> salvar(@RequestBody @Valid TopicoFormDto form) throws URISyntaxException {
 		Topico topico = topicoRepository.save(form.converte(cursoRepository));
 		return ResponseEntity.created(new URI("/topicos")).body(new TopicoDto(topico));
 	}
-
+	
+	@PutMapping(path = "/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,
+			@RequestBody @Valid AtualizacaoTopicoDto form) {
+		
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if(!topico.isPresent()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			Topico topicoAtualizado = form.atualiza(topico);
+			return ResponseEntity.ok().body(new TopicoDto(topicoAtualizado));
+		}
+		
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> deletar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		
+		if(!topico.isPresent()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			topicoRepository.delete(topico.get());
+			return ResponseEntity.ok().build();
+		}
+	}
+	
 }
