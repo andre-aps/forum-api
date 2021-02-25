031118c85d39efd87073e530d01ee37094f764ad
André <andre.gmaps@gmail.com>
2021-02-24 23:36:42 -0300

    adicionando autor do topico no form dto

--- src/main/java/br/com/alura/forum/dto/TopicoFormDto.java
+++ src/main/java/br/com/alura/forum/dto/TopicoFormDto.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.UsuarioRepository;
 
 public class TopicoFormDto {
 
@@ -17,14 +18,18 @@ public class TopicoFormDto {
 	@NotNull
 	@NotEmpty
 	private String nomeCurso;
+	@NotNull
+	@NotEmpty
+	private String autor;
 
 	public TopicoFormDto() {
 	}
 
-	public TopicoFormDto(String titulo, String mensagem, String nomeCurso) {
+	public TopicoFormDto(String titulo, String mensagem, String nomeCurso, String autor) {
 		this.titulo = titulo;
 		this.mensagem = mensagem;
 		this.nomeCurso = nomeCurso;
+		this.autor = autor;
 	}
 
 	public String getTitulo() {
@@ -39,8 +44,12 @@ public class TopicoFormDto {
 		return nomeCurso;
 	}
 
-	public Topico converte(CursoRepository cursoRepository) {
-		return new Topico(titulo, mensagem, cursoRepository.findByNome(nomeCurso));
+	public Topico converte(CursoRepository cursoRepository, UsuarioRepository usuarioRepository) {
+		return new Topico(titulo, mensagem, cursoRepository.findByNome(nomeCurso), usuarioRepository.findByNome(autor));
+	}
+
+	public String getAutor() {
+		return autor;
 	}
 
 }
