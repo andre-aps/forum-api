568ce050e892ca933a7afe6ee8792393d302fc4a
André <andre.gmaps@gmail.com>
2021-03-08 23:19:29 -0300

    adicionando novo método no usuario repository

--- src/main/java/br/com/alura/forum/repository/UsuarioRepository.java
+++ src/main/java/br/com/alura/forum/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.model.Usuario;
@@ -7,5 +9,7 @@ import br.com.alura.forum.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	Usuario findByNome(String autor);
+
+	Optional<Usuario> findByEmail(String email);
 
 }
