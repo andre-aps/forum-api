8cfff4aef2e22e9bfc0a6ec3b0dc6eac4c5ca6bb
André <andre.gmaps@gmail.com>
2021-03-17 22:47:40 -0300

    adicionando método de validação token e id usuario

--- src/main/java/br/com/alura/forum/service/TokenService.java
+++ src/main/java/br/com/alura/forum/service/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.forum.model.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -29,6 +30,20 @@ public class TokenService {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS256, secret)
 				.compact();
+	}
+
+	public Boolean isValido(String token) {
+		try {
+			Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Long getIdUsuario(String token) {
+		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
 	}
 	
 }
