4910244c0b70db25f13a96cbfb887bf6ad1650d1
André <andre.gmaps@gmail.com>
2021-03-22 18:27:20 -0300

    adicionando classe SwaggerConfigurations

---
+++ src/main/java/br/com/alura/forum/config/SwaggerConfigurations.java
@@ -0,0 +1,61 @@
+package br.com.alura.forum.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+	
+	@Bean
+	public Docket forumApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				  .apiInfo(metaInfo())
+			      .securityContexts(Arrays.asList(securityContext()))
+			      .securitySchemes(Arrays.asList(apiKey()))
+			      .select()
+			      .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+			      .paths(PathSelectors.any())
+			      .build();
+	}
+	
+	private ApiKey apiKey() { 
+	    return new ApiKey("JWT", "Authorization", "header"); 
+	}
+	
+	private SecurityContext securityContext() { 
+	    return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
+	} 
+
+	private List<SecurityReference> defaultAuth() { 
+	    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
+	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
+	    authorizationScopes[0] = authorizationScope; 
+	    return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
+	}
+	
+	private ApiInfo metaInfo() {
+		return new ApiInfoBuilder()
+                .title("Forum API Documentation")
+                .description("This pages documents Products API RESTful endpoints")
+                .version("1.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("André Pereira da Silva", "https://github.com/andre-aps", "andre.gmaps@gmail.com"))
+                .build();
+    }
+}
